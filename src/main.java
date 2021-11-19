aa4dd99f5b6816d26bfeb5f81ad15fe7b47dd7d0
YoussefWasfy <youssefwasfy@rocketmail.com>
2021-11-19 16:52:50 +0200

    first commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+
+    public static void main(String[] args) {
+        Matrix matrix = new Matrix();
+        String grid = matrix.GenGrid();
+        System.out.println(grid);
+
+
+    }
+}
